b06f83ee0b9d2cd9ee2d174a1f8b07440061c797
piyu0812 <167849694+piyu0812@users.noreply.github.com>
2024-06-27 13:39:30 +0530

    Add files via upload

---
+++ Pallondrome string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a=input("Enter a string")
+b=a[::-1]
+print(b)
+if a==b:
+    print("It's a pallandrome string")
+else:
+    print("Not a pallanndrome String")
+
+
+# In[ ]:
+
+
+
+
